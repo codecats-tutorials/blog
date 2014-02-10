f4bb543a84b0b886b656812e7ac69cdb8d94c754
ssstrz <codecats@gmail.com>
2014-02-10 21:01:20 +0100

    #testing-the-blog-entity-slugify-method

---
+++ src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blogger\BlogBundle\Tests\Entity;
+
+/**
+ * Description of BlogTest
+ *
+ * @author t
+ */
+class BlogTest extends \PHPUnit_TestCase
+{
+    //put your code here
+}
