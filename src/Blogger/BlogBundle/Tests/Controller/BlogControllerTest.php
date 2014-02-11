5c93f15cfe660737a910f21f0789bbe723f0859c
ssstrz <codecats@gmail.com>
2014-02-11 17:50:55 +0100

    #testing-the-twig-extension

--- src/Blogger/BlogBundle/Tests/Controller/BlogControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Blogger\BlogBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class BlogControllerTest extends WebTestCase
-{
-}
--- src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
+++ src/Blogger/BlogBundle/Tests/Entity/BlogTest.php
@@ -2,12 +2,40 @@
 
 namespace Blogger\BlogBundle\Tests\Entity;
 
+use Blogger\BlogBundle\Entity\Blog;
+
+
 /**
  * Description of BlogTest
  *
  * @author t
  */
-class BlogTest extends \PHPUnit_TestCase
+class BlogTest extends \PHPUnit_Framework_TestCase
 {
-    //put your code here
+    public function testTitle() 
+    {
+        $blog = new Blog();
+        
+        $blog->setTitle('A day with symfony');
+        $this->assertEquals('a-day-with-symfony', $blog->getSlug());
+    }
+    public function testSlug() 
+    {
+        $blog = new Blog();
+        $blog->setSlug('a DAY with symfony');
+        
+        $this->assertEquals('a-day-with-symfony', $blog->getSlug());
+    }
+    
+    public function testSlugify() 
+    {
+        $blog = new Blog();
+        $this->assertEquals('hello-world', $blog->slugify('Hello World'));
+        $this->assertEquals('a-day-with-symfony-2', $blog->slugify('A DaY With symFOny 2'));
+        $this->assertEquals('hello-world', $blog->slugify('Hello      World'));
+        $this->assertEquals('hello-world', $blog->slugify('Hello     World'));
+        $this->assertEquals('hello-world', $blog->slugify('    Hello WoRld    '));
+        $this->assertEquals('hello-world', $blog->slugify('Hello world '));
+        
+    }
 }
