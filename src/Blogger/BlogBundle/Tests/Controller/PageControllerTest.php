d8c7cb71548406ead459cf769c2aa91166314ce4
ssstrz <codecats@gmail.com>
2014-02-13 18:11:19 +0100

    #testing-the-contact-page

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -6,11 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PageControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndex() 
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/');
+        
+        $this->assertTrue($crawler->filter('article.blog')->count() > 0);
+        
+        $blogLink = $crawler->filter('article.blog h2 a')->first();
+        $blogTitle = $blogLink->text();
+
+        $crawler = $client->click($blogLink->link());
+        
+        $this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle . '")')->count());
+    }
+    public function testAbout()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/about');
+        
+        $this->assertEquals(1, $crawler->filter('h1:contains("About blog")')->count());
     }
 
 }
